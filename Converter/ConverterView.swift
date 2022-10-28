f19d1cfbd85225aae3f1ed69f0474b1b28ede2b0
vadinamo <vadinamo@gmail.com>
2022-10-28 12:42:33 +0300

    complete orientation

--- Converter/ConverterView.swift
+++ Converter/ConverterView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import AlertToast
 
 struct ConverterView: View {
+    @State private var orientation = UIDevice.current.orientation
+    
     @State var currentCategory: Int = 0
     
     @State var type1: Int = 0
@@ -19,10 +21,31 @@ struct ConverterView: View {
     @State var symbolLimitToast = false
     
     @State var input: String = "0"
-    
     var output: String {
         var value = Double(input) ?? 0
-        return String(value / valueCoefficients[currentCategory][type1] * valueCoefficients[currentCategory][type2])
+        var result = String(value / valueCoefficients[currentCategory][type1] * valueCoefficients[currentCategory][type2])
+        if String(result.suffix(2)) == ".0" {
+            result.removeLast(2)
+        }
+        
+        return result
+    }
+    
+    var width: CGFloat {
+        if UIScreen.main.bounds.height > UIScreen.main.bounds.width {
+            return UIScreen.main.bounds.width
+        }
+        else {
+            return UIScreen.main.bounds.height
+        }
+    }
+    var count: CGFloat {
+        if UIScreen.main.bounds.height > UIScreen.main.bounds.width {
+            return 4
+        }
+        else {
+            return 5
+        }
     }
     
     let buttons: [[Buttons]] = [
@@ -34,92 +57,122 @@ struct ConverterView: View {
     ]
     
     var body: some View {
-        VStack {
-            Picker("Category", selection: $currentCategory) {
-                ForEach(0..<unitCategories.count) {
-                    Text(unitCategories[$0])
-                        .font(.largeTitle)
-                }
-            }.pickerStyle(SegmentedPickerStyle())
-            
-            Spacer()
-            
-            HStack {
-                Picker("From type", selection: $type1) {
-                    ForEach(0..<unitTypes[currentCategory].count) {
-                        Text(unitTypes[currentCategory][$0])
-                            .font(.largeTitle)
-                    }
-                }
-                Spacer()
-                Text(input)
-                    .font(.system(size: 18)).padding()
-            }
-            
-            Spacer()
-            
-            HStack {
-                Picker("To type", selection: $type2) {
-                    ForEach(0..<unitTypes[currentCategory].count) {
-                        Text(unitTypes[currentCategory][$0])
-                            .font(.largeTitle)
-                    }
-                }
-                Spacer()
-                Text(output)
-                    .font(.system(size: 18)).padding()
-            }
-            
-            Spacer()
-            
-            ForEach(buttons, id: \.self) { row in
+        ZStack {
+            if orientation.isLandscape || UIScreen.main.bounds.height < UIScreen.main.bounds.width {
                 HStack {
-                    ForEach(row, id: \.self) { item in
-                        Button(action: {
-                            buttonTap(button: item)
-                        }, label: {
-                            item.buttonLabel
-                                .font(.largeTitle)
-                                .foregroundColor(.white)
-                                .frame(
-                                    width: self.buttonWidth(item: item),
-                                    height: self.buttonHeight(item: item))
-                                .background(item.buttonColor)
-                                .cornerRadius(self.buttonHeight(item: item))
-                        })
+                    VStack {
+                        inputs
                     }
-                }
+                    Spacer()
+                    VStack {
+                        keyboard
+                    }
+                }.padding(.all)
+            }
+            else if orientation.isPortrait || UIScreen.main.bounds.height > UIScreen.main.bounds.width {
+                VStack {
+                    inputs
+                    Spacer()
+                    keyboard
+                }.padding(.all)
             }
         }
-        .toast(isPresenting: $copyToast, duration: 1, tapToDismiss: false) {
-            AlertToast(displayMode: .hud, type: .regular, title: "Copied")
+        .onAppear {
+            orientation = UIApplication.shared.statusBarOrientation == .portrait ? UIDeviceOrientation.portrait : UIDeviceOrientation.landscapeLeft
         }
-        .toast(isPresenting: $invalidInputToast, duration: 1, tapToDismiss: false) {
-            AlertToast(displayMode: .hud, type: .regular, title: "хуйня переделывай")
+        .onRotate { newOrientation in
+            orientation = newOrientation
         }
-        .toast(isPresenting: $symbolLimitToast, duration: 1, tapToDismiss: false) {
-            AlertToast(displayMode: .hud, type: .error(.red), title: "Max input length is 15 symbols")
-        }
-        .padding(.all)
     }
     
     func buttonWidth(item: Buttons) -> CGFloat {
         if item == .zero {
-            return (UIScreen.main.bounds.width - (4 * 12)) / 4 * 2
+            return (width - (4 * 12)) / count * 2
         }
         else if item == .left || item == .right {
-            return (UIScreen.main.bounds.width - (4 * 12)) / 4 * 3 / 2
+            return (width - (4 * 12)) / count * 3 / 2
         }
             
-        return (UIScreen.main.bounds.width - (5 * 12)) / 4
+        return (width - (5 * 12)) / count
     }
     
     func buttonHeight(item: Buttons) -> CGFloat {
-        return (UIScreen.main.bounds.width - (5 * 12)) / 4
+        return (width - (5 * 12)) / count
     }
     
     func clearInput() {
         self.input = "0"
+    }
+    
+    @ViewBuilder
+    var inputs: some View {
+        Picker("Category", selection: $currentCategory) {
+            ForEach(0..<unitCategories.count) {
+                Text(unitCategories[$0])
+                    .font(.largeTitle)
+            }
+        }.pickerStyle(SegmentedPickerStyle())
+        .toast(isPresenting: $copyToast, duration: 1, tapToDismiss: false) {
+            AlertToast(displayMode: .hud, type: .regular, title: "Copied")
+        }
+        .toast(isPresenting: $invalidInputToast, duration: 2, tapToDismiss: false) {
+            AlertToast(displayMode: .hud, type: .regular, title: "хуйня переделывай")
+        }
+        .toast(isPresenting: $symbolLimitToast, duration: 3, tapToDismiss: false) {
+            AlertToast(displayMode: .hud, type: .error(.red), title: "Max input length is 15 symbols")
+        }
+        
+        Spacer()
+        
+        HStack {
+            Picker("From type", selection: $type1) {
+                ForEach(0..<unitTypes[currentCategory].count) {
+                    Text(unitTypes[currentCategory][$0])
+                        .font(.largeTitle)
+                }
+            }
+            Spacer()
+            Text(input)
+                .font(.system(size: 18)).padding()
+        }
+        
+        Spacer()
+        
+        HStack {
+            Picker("To type", selection: $type2) {
+                ForEach(0..<unitTypes[currentCategory].count) {
+                    Text(unitTypes[currentCategory][$0])
+                        .font(.largeTitle)
+                }
+            }
+            Spacer()
+            Text(output)
+                .font(.system(size: 18)).padding()
+        }
+        
+        Spacer()
+    }
+    
+    @ViewBuilder
+    var keyboard: some View {
+        ForEach(buttons, id: \.self) { row in
+            HStack {
+                ForEach(row, id: \.self) { item in
+                    Button(action: {
+                        buttonTap(button: item)
+                    }, label: {
+                        item.buttonLabel
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .frame(
+                                width: self.buttonWidth(item: item),
+                                height: self.buttonHeight(item: item))
+                            .background(item.buttonColor)
+                            .cornerRadius(self.buttonHeight(item: item))
+                    })
+                }
+            }
+        }
     }
     
     func buttonTap(button: Buttons) {
@@ -131,6 +184,9 @@ struct ConverterView: View {
         case .swap:
             if output.count < 15 {
                 self.input = output
+            }
+            else {
+                self.symbolLimitToast.toggle()
             }
             break
         case .copy:
@@ -186,11 +242,32 @@ struct ConverterView: View {
                     self.input = "\(self.input)\( button.rawValue)"
                 }
             }
+            else {
+                self.symbolLimitToast.toggle()
+            }
             break
         }
     }
 }
 
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+// A View wrapper to make the modifier easier to use
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
 
 
 
