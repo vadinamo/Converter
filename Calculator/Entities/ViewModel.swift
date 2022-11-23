e866e0880abe6f1f3d25835c149de813591cbab9
vadinamo <vadinamo@gmail.com>
2022-11-24 00:41:00 +0300

    full cursor + bit input fix

--- Calculator/Entities/ViewModel.swift
+++ Calculator/Entities/ViewModel.swift
@@ -22,33 +22,70 @@ class ViewModel: ObservableObject {
     
     func moveCursorLeft() {
         if cursorIndex > 0 {
-            var characters = Array(input)
-            characters.swapAt(cursorIndex - 1, cursorIndex)
-            self.input = String(characters)
-            self.cursorIndex -= 1
+            self.input = input.replacingOccurrences(of: cursorSymbol, with: "")
+            
+            var i = cursorIndex - 1
+            if !input[i].isNumber && !several_items_operations.contains(where: {$0 == input[i]}) {
+                while i > 0 {
+                    if ((input[i] == "(" || input[i] == ")") && i != cursorIndex - 1) || several_items_operations.contains(where: {$0 == input[i]}) || input[i].isNumber {
+                        i += 1
+                        break
+                    }
+                    
+                    i -= 1
+                }
+            }
+            
+            let start = input.prefix(i)
+            let end = input.suffix(input.count - i)
+            
+            self.input = start + cursorSymbol + end
+            self.cursorIndex = i
         }
     }
     
     func moveCursorRight() {
         if cursorIndex < input.replacingOccurrences(of: cursorSymbol, with: "").count {
-            var characters = Array(input)
-            characters.swapAt(cursorIndex, cursorIndex + 1)
-            self.input = String(characters)
-            self.cursorIndex += 1
+            self.input = input.replacingOccurrences(of: cursorSymbol, with: "")
+            
+            var i = cursorIndex + 1
+            if !several_items_operations.contains(where: {$0 == input[i]}) && !several_items_operations.contains(where: {$0 == input[cursorIndex]}) {
+                while i < input.count {
+                    if input[i].isNumber || input[i] == ")" {
+                        break
+                    }
+                    else if input[i] == "(" {
+                        i += 1
+                        break
+                    }
+                    i += 1
+                }
+            }
+            
+            let start = input.prefix(i)
+            let end = input.suffix(input.count - i)
+            
+            self.input = start + cursorSymbol + end
+            self.cursorIndex = i
         }
     }
     
     func inputValue(value: String) {
-        if (several_items_operations.contains(where: {$0 == value}) || value == "!") && (cursorIndex == 0 ||
-            several_items_operations.contains(where: {$0 == input[cursorIndex - 1]})) {
-            return
+        if (several_items_operations.contains(where: {$0 == value}) || value == "!") {
+            if cursorIndex == 0 || several_items_operations.contains(where: {$0 == input[cursorIndex - 1]}) {
+                return
+            }
+            
+            else if input.replacingOccurrences(of: cursorSymbol, with: "")[cursorIndex - 1] == "(" && value != "-"{
+                return
+            }
         }
         
         var val = value
         self.input = input.replacingOccurrences(of: cursorSymbol, with: "")
         let start = input.prefix(cursorIndex)
         let end = input.suffix(input.count - cursorIndex)
-    
+        
         if !val.isNumber && val.needBracket {
             val += "("
         }
